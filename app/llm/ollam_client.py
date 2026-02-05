4e1e836acf4dd95b7627cca08387ccc61126efa5
YaredAlex <yareda25@gmail.com>
2026-02-05 16:19:47 +0300

    feat: using ollama to inference request

--- app/llm/ollam_client.py
+++ app/llm/ollam_client.py
@@ -1,9 +1,11 @@
 import requests
 import json
+from ollama import Client
 
 OLLAMA_URL = "http://localhost:11434/api/generate"
 # MODEL = "llama3.1:8b"
 MODEL = "gpt-oss:20b-cloud"
+client = Client(host='http://localhost:11434')
 
 def call_llm(prompt: str) -> str:
     response = requests.post(
@@ -18,3 +20,13 @@ def call_llm(prompt: str) -> str:
     )
     response.raise_for_status()
     return response.json()["response"]
+
+def ollama_client(messages:list[str])->str:
+    response = client.chat(model=MODEL,messages=[
+        {
+            "role":"user",
+            "content":""
+        }
+    ])
+
+    return response
