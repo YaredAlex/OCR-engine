d0da7e090b5c4045ff35e6dba461354f476aec5f
YaredAlex <yareda25@gmail.com>
2026-02-09 10:36:44 +0300

    feat: method for creating multiple doc prompts

--- app/document/classifier.py
+++ app/document/classifier.py
@@ -1,8 +1,8 @@
 
-from llm.ollam_client import call_llm
+from llm.ollam_client import call_llm, ollama_chain
 
 
-def classify_document(ocr_text: str) -> str:
+def classify_document_single(ocr_text: str) -> str:
     prompt = f"""
 You are a document classification system.
 
@@ -22,3 +22,27 @@ Text:
 Respond with ONLY the document type.
 """
     return call_llm(prompt).strip()
+
+
+def classify_documents(ocr_texts: list[str]) -> list[str]:
+    
+    prompt = lambda x: f"""
+You are a document classification system.
+
+Classify the document into one of:
+- National ID
+- Passport
+- Driver License
+- Invoice
+- Bank Statement
+- License compitency
+- Commercial registration
+- Unknown
+
+Text:
+{x}
+
+Respond with ONLY the document type.
+"""
+    
+    return ollama_chain([prompt(doc) for doc in ocr_texts])
