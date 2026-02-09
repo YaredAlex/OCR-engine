9aadebc2bd1ef5a544b34c228ba5c71659db8be1
YaredAlex <yareda25@gmail.com>
2026-02-09 10:38:05 +0300

    feat(add): langchain ollama for batch processing

--- app/llm/ollam_client.py
+++ app/llm/ollam_client.py
@@ -1,12 +1,19 @@
 import requests
 import json
 from ollama import Client
+from langchain_ollama.llms import OllamaLLM
 
 OLLAMA_URL = "http://localhost:11434/api/generate"
 # MODEL = "llama3.1:8b"
 MODEL = "gpt-oss:20b-cloud"
 client = Client(host='http://localhost:11434')
 
+llm_chain = OllamaLLM(
+    model=MODEL, 
+    temperature=0.0,
+)
+
+# method 1 using http request 
 def call_llm(prompt: str) -> str:
     response = requests.post(
         OLLAMA_URL,
@@ -21,12 +28,18 @@ def call_llm(prompt: str) -> str:
     response.raise_for_status()
     return response.json()["response"]
 
-def ollama_client(messages:list[str])->str:
+# method 2 using ollama client
+def ollama_client(message:str)->str:
+    # TODO convert message to list
     response = client.chat(model=MODEL,messages=[
         {
             "role":"user",
-            "content":""
+            "content":f"{message}"
         }
     ])
 
     return response
+
+# method 3 using langchain
+def ollama_chain(messages:list[str])->list[str]:
+    return llm_chain.batch(messages)
