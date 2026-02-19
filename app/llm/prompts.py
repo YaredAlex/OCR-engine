df39fe401eea1e081e0421443b114b92e256bd0b
YaredAlex <yareda25@gmail.com>
2026-02-19 15:53:49 +0300

    mod: added strict rule to start with curly brace

--- app/llm/prompts.py
+++ app/llm/prompts.py
@@ -4,14 +4,16 @@ You are an OCR post-processing AI.
 
 Document type: {doc_type}
 
-Extract the information and return STRICT JSON
+Extract the information and return STRICT JSON and START WIHT CURLY BRACES
 following this schema:
+<schema>
 {schema}
-
+</schema>
 Rules:
 - If field not found, use null
 - No explanation
 - JSON only
+- I No schema matches make a schema that fits the document
 
 OCR TEXT:
 {text}
