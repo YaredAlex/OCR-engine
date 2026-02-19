9f23493b70d334a9302d0c01ab46d4f496c80993
YaredAlex <yareda25@gmail.com>
2026-02-19 15:48:52 +0300

    refactor: remove debugging line

--- app/ocr/preprocess.py
+++ app/ocr/preprocess.py
@@ -11,8 +11,4 @@ def preprocess_image(path: str) -> str:
     # )
     output_path = path.replace(".jpg", "_clean.jpg")
     cv2.imwrite(output_path, img)
-    save_path = Path("output/clean")
-    save_path = save_path / f"{str(uuid.uuid4())}.jpg"
-    # print("save path ",str(save_path))
-    cv2.imwrite(str(save_path),img)
     return output_path
