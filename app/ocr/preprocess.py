518a817ebf0844f7b2add220f5663b99901aa701
YaredAlex <yareda25@gmail.com>
2026-02-19 14:31:55 +0300

    fix: remove adaptiveThreshold (causing noise on image)

--- app/ocr/preprocess.py
+++ app/ocr/preprocess.py
@@ -1,13 +1,18 @@
 import cv2
 import numpy as np
-
+from pathlib import Path
+import uuid
 def preprocess_image(path: str) -> str:
-    img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-    img = cv2.adaptiveThreshold(
-        img, 255,
-        cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-        cv2.THRESH_BINARY, 31, 2
-    )
+    img = cv2.imread(path)
+    # img = cv2.adaptiveThreshold(
+    #     img, 255,
+    #     cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+    #     cv2.THRESH_BINARY, 31, 2
+    # )
     output_path = path.replace(".jpg", "_clean.jpg")
     cv2.imwrite(output_path, img)
+    save_path = Path("output/clean")
+    save_path = save_path / f"{str(uuid.uuid4())}.jpg"
+    # print("save path ",str(save_path))
+    cv2.imwrite(str(save_path),img)
     return output_path
